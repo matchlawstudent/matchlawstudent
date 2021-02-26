5558e89d493ad4dab4c9ddd2e7cdb3152013ffde
samuellevy <slevy3@uw.edu>
2021-02-25 20:06:17 -0800

    photos working

--- src/static/team-page-info.js
+++ src/static/team-page-info.js
@@ -1,15 +1,23 @@
+import flower from '../images/flower.jpg';
+import sam from '../images/sam.jpg';
+import khai from '../images/khai.jpg';
+import jason from '../images/jason.jpg';
+import crystal from '../images/crystal.jpg';
+
+
+
 export default {
     teamData: [
         {
             name: "Flower Hua",
-            image: "/team-photos/flower.jpg",
+            image: flower,
             bio: "Research Assistant at Shanze Partners PLLC. I am passionated about the intersection of technology and " +
             "policy making. I believe tech should help people to bridge the gap between poverty, accessibility and diversity.",
             linkedin: "https://www.linkedin.com/in/zhuofan-flower-hua/"
         },
         {
             name: "Crystal Bai",
-            image: "/team-photos/crystal.jpg",
+            image: crystal,
             bio: "Recent graduate in Communication from University of Washington. " +
             "I’m always excited to meet new people, listen to their stories, and seek the most interesting and efficient way to communicate.",
             linkedin: "https://www.linkedin.com/in/bing-crystal-bai/"
@@ -17,7 +25,7 @@ export default {
         },
         {
             name: "Jason Huang",
-            image: "/team-photos/jason.jpg",
+            image: jason,
             bio: "Jiazheng(Jason) is a student at the University of Arizona studying Computer Science. After a few years of study " +
             "in computer science, he realized his passion lies in a field that combines UX and programming, which is what he is trying " +
             "to achieve in the future. Apparently, he is cat person",
@@ -26,14 +34,14 @@ export default {
         },
         {
             name: "Suyang Chen",
-            image: "/team-photos/headshot.jpg",
+            image: "",
             bio: "",
             linkedin: "https://www.linkedin.com/in/samuel-levy-69014b1a2/"
 
         },
         {
             name: "Khai Tran",
-            image: "/team-photos/khai.jpg",
+            image: khai,
             bio: "I’m Khai Tran. I proud that I become a Full-Stack Software Developer at MatchLawsStudent. My goal is to do "+
             "as much as possible to finish the job. I am very happy when I see the good result from work.",
             linkedin: "https://www.linkedin.com/in/khaithetran/"
@@ -42,7 +50,7 @@ export default {
 
         {
             name: "Samuel Levy",
-            image: "/team-photos/sam.jpg",
+            image: sam,
             bio: "I am a second-year student at UW studying " +
             "computer science. I am passionate about the intersection of technology, " +
             "ethics, law, and social/economic problems. In my free time, I enjoy running, " +
