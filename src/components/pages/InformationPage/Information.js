afad624bcc45d333448e2cdb36aee5e00a230d56
samuellevy <slevy3@uw.edu>
2021-03-12 15:04:39 -0800

    Adding Juno to team page

--- src/components/pages/InformationPage/Information.js
+++ src/components/pages/InformationPage/Information.js
@@ -3,8 +3,9 @@ import { Grid, } from '@material-ui/core'
 import IconButton from '@material-ui/core/IconButton'
 import './Information.css';
 import Resources from '../../../static/resources';
-
-
+import questionsAndAnswers from '../../../static/q-and-a'; 
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent'; 
 
 export default function Information() {
   return (
@@ -15,24 +16,43 @@ export default function Information() {
         justify="space-evenly"
         alignItems="flex-start" 
         style={{marginBottom: "20px"}}>          
-        <Grid item xs={12} sm={4} className="info-grid" id="info-grid-one">
+        <Grid item xs={12} sm={5} md={4} className="info-grid" id="info-grid-one">
             <h2 className="title">Free Legal Help in WA</h2>
             {Resources.freeLegalHelp.map((resource) => (
               <p key={resource.text} ><a  href={resource.link} className="link-style" target="_blank">{resource.text}</a></p>
             ))}
         </Grid>
-        <Grid item xs={12} sm={3} className="info-grid" id="info-grid-two">
+        <Grid item xs={12} sm={5} md={3} className="info-grid" id="info-grid-two">
             <h2 className="title">Legal Information & Resources</h2>
             {Resources.legalInfo.map((resource) => (
               <p key={resource.text} ><a href={resource.link} className="link-style" target="_blank">{resource.text}</a></p>
             ))}
         </Grid>
-        <Grid item xs={12} sm={4} className="info-grid" id="info-grid-three">
+        <Grid item xs={12} sm={12} md={4} className="info-grid" id="info-grid-three">
             <h2 className="title">Covid Resources</h2>
             {Resources.covidInfo.map((resource) => (
               <p key={resource.text}><a  href={resource.link} className="link-style" target="_blank">{resource.text}</a></p>
             ))}
         </Grid>
+      </Grid>
+      <h1 style={{textDecoration: "underline", marginBottom: "20px"}}>Q & A</h1>
+      <Grid container
+      direction="row"
+      justify="space-around"
+      alignItems="flex-start" 
+      style={{marginBottom: "20px"}}>
+          {
+            questionsAndAnswers.questionsAndAnswers.map((lawCat) => (
+              <Grid item xs={12} sm={6} md={2}>
+                <Card style={{  background: "#384728", color: "white",}}>
+                  <CardContent ><h3>{lawCat.name}</h3></CardContent>
+                  <CardContent style={{textAlign: "left"}}>
+                    {lawCat.description}
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))
+          }
       </Grid>
     </div>
   );
--- src/components/pages/TeamPage/Team.js
+++ src/components/pages/TeamPage/Team.js
@@ -7,30 +7,39 @@ import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import IconButton from '@material-ui/core/IconButton'
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import './Team.css';
 
 
 
 export default function Team() {
   return (
-    <div className="root">
+    <div className="root" style={{ padding: 20 }}>
       <h1 className="team-header">Our Team</h1>
-      <Grid container spacing={0} 
+      <Grid container spacing={4} 
       alignItems="flex-start"
-      justify="center">          
+      justify="space-evenly"
+      className="team-container">          
         {TeamData.teamData.map((member) => (
-          <Grid item xs={12} sm={4} className="person" style={{marginBottom: "50px"}} key={member.name}>
-            <Avatar src={member.image} className="avatar" style={{width: "150px", height: "150px"}}></Avatar>
-            <p className="team-text">
-            <span style={{fontWeight: "bold", fontSize: "15px"}}>{member.name}</span> 
-            <a href={member.linkedin} target="_blank">
-              <IconButton variant="link" color="primary" component="span" >
-                <LinkedInIcon style={{fontSize: "20px", }}/>
-              </IconButton>
-            </a>
-            <br></br><br></br>
-              {member.bio}
-            </p>
+          <Grid item  xs={12} sm={6} md={4} lg={3} xl={2} className="person" style={{marginBottom: "50px"}} key={member.name}>
+            <Card className="team-card">
+            <CardContent>
+              <Avatar src={member.image} className="avatar" style={{width: "200px", height: "200px"}}></Avatar>
+              <p className="team-text">
+              <span style={{fontWeight: "bold", fontSize: "15px"}}>{member.name}</span> 
+              <a href={member.linkedin} target="_blank">
+                <IconButton variant="link" color="primary" component="span" >
+                  <LinkedInIcon style={{fontSize: "20px", }}/>
+                </IconButton>
+              </a>
+              <br></br><br></br>
+                {member.bio}
+              </p>
+            </CardContent>
+            </Card>
           </Grid>
         ))}
       </Grid>
