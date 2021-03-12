c74e2eb3b2af4884162caaa7d9844fe84a82dc46
OyunominJuno <oyunominmun@gmail.com>
2021-03-12 14:47:10 -0800

    added the plus button

--- src/components/pages/Dashboard/SquareInfo.js
+++ src/components/pages/Dashboard/SquareInfo.js
@@ -4,7 +4,10 @@ import './DashboardUser.css';
 export default function SquareInfo(props) {
     return (
         <div className='container-square'>
-            <h1 className='title'>{props.title}</h1>
+            <div className='titleNButton'>
+                <h1 className='title'>{props.title}</h1>
+                <button className='plusButton'> + </button>
+            </div>
             <ul className='content-list-container'> 
             {props.contentList.map((item, index) => (
                 <li key={index} className='content-list'>{item}</li>
